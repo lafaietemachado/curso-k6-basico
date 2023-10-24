9c8d09d7c98d2300fd78365ce5c136e3d49c0774
Lafaiete Rodrigues Machado <lafaiete.machado@gmail.com>
2023-10-24 09:44:56 -0300

    curso-k6-basico

---
+++ test.sh
@@ -0,0 +1,4 @@
+curl --request POST \
+    --url http://localhost:3333/signup \
+    --header 'Content-Type: application/json' \
+    --data '{"email": "lafaiete@qacademy.io", "password": "pwd123"}'
